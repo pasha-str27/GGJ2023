339dfd2b18b9833c1a817b61e750bc95a08cd07d
Vitalii Boiko <boiko_ak18@nuwm.edu.ua>
2023-01-31 18:28:17 +0200

    added camera controller

--- Assets/_Scripts/Utility/CameraController.cs
+++ Assets/_Scripts/Utility/CameraController.cs
@@ -14,11 +14,21 @@ public class CameraController : SingletonComponent<CameraController>
     [SerializeField] private bool useSafeMode = true;
 
     [Header("Move")]
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float toGridMoveSpeed;
+    [SerializeField] private float toTreeMoveSpeed;
+    [SerializeField] private float treeScaleSpeed;
+
+    [SerializeField] private Ease gridEase;
+    [SerializeField] private Ease treeEase;
+    [SerializeField] private Ease treeScaleEase;
+
 
     [Header("Temp Targets")]
     [SerializeField] private Transform tree;
     [SerializeField] private Transform grid;
+
+    [Header("Score")]
+    [SerializeField] private int score;
 
     private Transform _treeCamTform;
     private Transform _backCamTform;
@@ -50,13 +60,31 @@ public class CameraController : SingletonComponent<CameraController>
         _gridTform = grid.transform;
     }
 
+    [ContextMenu("ShowTree")]
     public void ShowTree()
     {
-
+        _treeCamTform.DOMoveY(_treeTform.position.y, toTreeMoveSpeed).
+            SetEase(treeEase).
+            OnComplete(ScaleBackground);
+        Debug.Log("Showed tree");
     }
 
+    private void ScaleBackground()
+    {
+        var scale = score * 0.01f;
+        if (scale > 1f)
+        {
+            var camStartPos = _backCamTform.position;
+            _backCamTform.DOMove(new Vector3(camStartPos.x, camStartPos.y, -10 - 10 * scale), treeScaleSpeed).
+                SetEase(treeScaleEase);
+        }
+    }
+
+    [ContextMenu("ShowGrid")]
     public void ShowGrid()
     {
-        _treeCamTform.DOMoveY(grid.transform.position.y, moveSpeed);
+        _treeCamTform.DOMoveY(_gridTform.position.y, toGridMoveSpeed).
+            SetEase(gridEase);
+        Debug.Log("Showed grid");
     }
 }
