966aea3aaed450a544df45b571e71337b1b37061
paveleon <54797658+pasha-str27@users.noreply.github.com>
2023-02-01 15:38:52 +0200

    Squashed commit of the following:
    commit 9f3ee6d849c67540ae0a769dfb3b351a0101fdf6
    Author: RaPloky <raplokygamingone@gmail.com>
    Date:   Tue Jan 31 02:57:36 2023 +0200
    
        workspace init

---
+++ Assets/_Scripts/GameApplication.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class GameApplication : MonoBehaviour
+    {
+        public GameModel Model;
+        public GameView View;
+        public GameController Controller;
+
+        private void Start()
+        {
+
+        }
+    }
+}
